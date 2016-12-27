51d8102ad87a29b11bf53d10751b992ed2ffe1f4
Leif Bladt <leif.bladt@gmx.de>
2016-12-27 15:20:21 +0100

    Set CoreXY kinematic

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,7 +491,7 @@
 
 // Uncomment one of these options to enable CoreXY, CoreXZ, or CoreYZ kinematics
 // either in the usual order or reversed
-//#define COREXY
+#define COREXY
 //#define COREXZ
 //#define COREYZ
 //#define COREYX
@@ -849,7 +849,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 
 // @section extruder
