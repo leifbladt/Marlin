ac2f6271e027bb571be2c02e577d09a8641b37e3
Leif Bladt <leif.bladt@gmx.de>
2017-02-19 17:37:07 +0100

    Adjust bed measurements

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -885,15 +885,15 @@
 
 // The size of the print bed
 #define X_BED_SIZE 210
-#define Y_BED_SIZE 210
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -16
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 220
+#define Z_MAX_POS 222
 
 /**
  * Software Endstops
